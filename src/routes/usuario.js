52a5d64814331df7d655517125b8b2a6e8dba788
irudigio <irudigio@gmail.com>
2022-06-15 21:21:30 -0300

    agrega validaciones

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,11 +2,21 @@
 const express = require('express');
 const router = express.Router();
 
+const {body} = require("express-validator");
+
 // ************ Controller Require ************
 const usuarioController = require('../controllers/usuarioController');
 
+const validator = [
+    body('name').notEmpty().withMessage('Debes completar el nombre'),
+    body('email').notEmpty().withMessage('Debes completar el email'),
+    body('password').notEmpty().withMessage('Debes completar la contraseña'),
+    body('confirm').notEmpty().withMessage('Debes confirmar la constraseña'),
+
+]
+
 router.get('/', usuarioController.index); 
 
-/*router.???('/search', mainController.search); */
+router.post('/',validator, usuarioController.processForm); 
 
 module.exports = router;
